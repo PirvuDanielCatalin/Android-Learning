621e24eea4cba3331b1cd551301918ebf7f3894c
Pirvu Daniel <danut_pirvu2008@yahoo.com>
2019-04-17 16:42:39 +0300

    Shared Preferences, RefreshLayout and more fragments

--- app/src/main/java/com/example/project/helpers/Constants.java
+++ app/src/main/java/com/example/project/helpers/Constants.java
@@ -2,6 +2,7 @@ package com.example.project.helpers;
 
 public class Constants {
     public static String DB_NAME = "localDB";
+    public static String SHARED_PREFERENCES_NAME = "localSH";
 
     public static String REMOVED_BOOK_MSG = "Cartea a fost eliminată de la favorite!";
     public static String ADDED_BOOK_MSG = "Cartea a fost adăugată la favorite!";
